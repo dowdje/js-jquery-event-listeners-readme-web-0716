6318cd7fa9b7da6a2f6ac3e1c10ea2490abd3d1b
dowdje <dowdje89@gmail.com>
2016-08-31 18:48:40 -0400

    Used Event Listeners

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').on('click', function(){alert('hey'); return})
+}
 
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+$(document).ready(function(){
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 // call functions here
 
 });
